92c6adfebb18531c5429e03575ccfc64ffb1bd64
cwllmsn <cwllmsn@outlook.com>
2025-07-11 20:47:41 +0100

    Optimise screen refresh

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +17,8 @@ def main():
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000.0  # Convert milliseconds to seconds
     
  
     
