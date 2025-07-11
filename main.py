517c891b27d78e12d37a7f071e078315e9015e18
cwllmsn <cwllmsn@outlook.com>
2025-07-11 21:29:43 +0100

    Refactor to use containers

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0  # Convert milliseconds to seconds
 
